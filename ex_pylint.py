80fac06f4e228062f3292af79c44168d8fe2a8c8
Anderson Fernandes <anderson.fernandes96@outlook.com>
2025-11-12 22:16:26 -0300

    config: adding pylint on project (part 2)

--- ex_pylint.py
+++ ex_pylint.py
@@ -1,7 +1,8 @@
-''' Alguma documentação sobre o arquivo '''
-
 print("Ola Mundo")
 
 def minha_funcao():
-    ''' alguma coisa '''
-    print("Estou na minha função")
+    print("Estou na minha funcao")
+
+# snake_case -> variáveis e funções
+# camelCase -> Não consta
+# PascalCase -> nomes de classes
