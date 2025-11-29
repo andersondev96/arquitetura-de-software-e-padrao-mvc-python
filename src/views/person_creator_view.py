a4953404e86f9b647e6ae41aa019eee36c44e1e1
Anderson Fernandes <anderson.fernandes96@outlook.com>
2025-11-29 12:20:32 -0300

    feat: implementing the rest of the views

--- src/views/person_creator_view.py
+++ src/views/person_creator_view.py
@@ -1,5 +1,4 @@
 from src.controllers.interfaces.person_creator_controller import PersonCreatorControllerInterface
-
 from .http_types.http_request import HttpRequest
 from .http_types.http_response import HttpResponse
 from .interfaces.view_interface import ViewInterface
